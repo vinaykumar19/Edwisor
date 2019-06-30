fed7f14fe9e328253ba155dc6edf6d9b45136bb0
vinay kumar <vinaykugupta.000@gmail.com>
2019-06-30 12:27:57 +0530

    my first commit

---
+++ practice.js
@@ -0,0 +1,18 @@
+
+	<script type="text/javascript">
+
+//food items in a menu
+var foodItem={
+
+"itemType":"coffee"
+"cost":230
+
+}
+ 
+var costOfTheItem= foodItem["cost"] ;
+alert(costOfTheItem);
+		
+
+
+
+</script>
